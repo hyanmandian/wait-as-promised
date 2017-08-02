828bfcaf6a41d0b2aeb84885da67c2dc522c777c
Alex Rocha <alex.rochas@yahoo.com.br>
2017-08-01 22:19:11 -0300

    feat: add timeout feature

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 'use strict';
 
-module.exports = predicate => new Promise(resolve => {
-	const loop = setInterval(() => {
-		if (predicate()) {
-			clearInterval(loop);
-			resolve();
-		}
-	});
+module.exports = (predicate, {timeout} = {timeout: 5000}) => new Promise(resolve => {
+  const racingPromises = [];
+
+  const intervalPromise = new Promise(resolve => {
+    const loop = setInterval(() => {
+      if (predicate()) {
+        clearInterval(loop);
+        resolve('done');
+      }
+    });
+  });
+  racingPromises.push(intervalPromise);
+
+  if (timeout >= 0) {
+    const timeoutPromise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const timeoutMessage = `function timed out after ${timeout} milliseconds`;
+        reject(new Error(timeoutMessage));
+      }, timeout);
+    });
+    racingPromises.push(timeoutPromise);
+  }
+
+  return resolve(Promise.race(racingPromises));
 });
--- test.js
+++ test.js
@@ -1,14 +1,66 @@
 import test from 'ava';
 import wait from '.';
 
-test('wait resolve', async t => {
-	let value = false;
+test('should resolve', async t => {
+  let value = false;
 
-	const predicate = () => Boolean(value);
+  const predicate = () => Boolean(value);
 
-	setTimeout(() => {
-		value = true;
-	});
+  setTimeout(() => {
+    value = true;
+  });
 
-	await wait(predicate).then(() => t.pass());
+  await wait(predicate).then(() => t.pass());
+});
+
+test('should timeout', async t => {
+  let value = false;
+
+  const predicate = () => Boolean(value);
+
+  setTimeout(() => {
+    value = true;
+  }, 5100);
+
+  const error = await t.throws(wait(predicate, {timeout: 500}));
+
+  t.is(error.message, 'function timed out after 500 milliseconds');
+});
+
+test('should not timeout', async t => {
+  let value = false;
+
+  const predicate = () => Boolean(value);
+
+  setTimeout(() => {
+    value = true;
+  }, 200);
+
+  await wait(predicate, {timeout: 500}).then(() => t.pass());
+});
+
+test('should timeout by default after 5000 milliseconds', async t => {
+  let value = false;
+
+  const predicate = () => Boolean(value);
+
+  setTimeout(() => {
+    value = true;
+  }, 5100);
+
+  const error = await t.throws(wait(predicate));
+
+  t.is(error.message, 'function timed out after 5000 milliseconds');
+});
+
+test('should not timeout if timeout value is lower than zero', async t => {
+  let value = false;
+
+  const predicate = () => Boolean(value);
+
+  setTimeout(() => {
+    value = true;
+  }, 5100);
+
+  await wait(predicate, {timeout: -1}).then(() => t.pass());
 });
