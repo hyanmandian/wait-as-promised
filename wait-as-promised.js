2fd35fc3648cc6f29bde3a870a9c982aab9d0f9e
Hyan Mandian <hyanmandian@hotmail.com>
2018-03-29 14:15:46 -0300

    [refactor]

--- wait-as-promised.js
+++ wait-as-promised.js
@@ -1,27 +1,24 @@
 export default (predicate, {timeout} = {timeout: 5000}) => new Promise(resolve => {
-	const racingPromises = [];
+  const racingPromises = [];
 
-	const intervalPromise = new Promise(resolve => {
-		const loop = setInterval(() => {
-			if (predicate()) {
-				clearInterval(loop);
-				resolve('done');
-			}
-		});
-	});
+  const intervalPromise = new Promise(resolve => {
+    const loop = setInterval(() => {
+      if (predicate()) {
+        clearInterval(loop);
+        resolve();
+      }
+    });
+  });
 
-	racingPromises.push(intervalPromise);
+  racingPromises.push(intervalPromise);
 
-	if (timeout >= 0) {
-		const timeoutPromise = new Promise((resolve, reject) => {
-			setTimeout(() => {
-				const timeoutMessage = `function timed out after ${timeout} milliseconds`;
-				reject(new Error(timeoutMessage));
-			}, timeout);
-		});
+  if (timeout >= 0) {
+    const timeoutPromise = new Promise((resolve, reject) => {
+      setTimeout(() => reject(new Error(`function timed out after ${timeout} milliseconds`)), timeout);
+    });
 
-		racingPromises.push(timeoutPromise);
-	}
+    racingPromises.push(timeoutPromise);
+  }
 
-	return resolve(Promise.race(racingPromises));
+  return resolve(Promise.race(racingPromises));
 });
