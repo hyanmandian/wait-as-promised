bc7d13afbcd196d579ee83fa6cb5354c78bf0f61
Hyan Mandian <hyanmandian@hotmail.com>
2018-03-29 14:21:29 -0300

    [fix] Lint errors

--- wait-as-promised.js
+++ wait-as-promised.js
@@ -1,24 +1,24 @@
-export default (predicate, {timeout} = {timeout: 5000}) => new Promise(resolve => {
-  const racingPromises = [];
+module.exports = (predicate, {timeout} = {timeout: 5000}) => new Promise(resolve => {
+	const racingPromises = [];
 
-  const intervalPromise = new Promise(resolve => {
-    const loop = setInterval(() => {
-      if (predicate()) {
-        clearInterval(loop);
-        resolve();
-      }
-    });
-  });
+	const intervalPromise = new Promise(resolve => {
+		const loop = setInterval(() => {
+			if (predicate()) {
+				clearInterval(loop);
+				resolve();
+			}
+		});
+	});
 
-  racingPromises.push(intervalPromise);
+	racingPromises.push(intervalPromise);
 
-  if (timeout >= 0) {
-    const timeoutPromise = new Promise((resolve, reject) => {
-      setTimeout(() => reject(new Error(`function timed out after ${timeout} milliseconds`)), timeout);
-    });
+	if (timeout >= 0) {
+		const timeoutPromise = new Promise((resolve, reject) => {
+			setTimeout(() => reject(new Error(`function timed out after ${timeout} milliseconds`)), timeout);
+		});
 
-    racingPromises.push(timeoutPromise);
-  }
+		racingPromises.push(timeoutPromise);
+	}
 
-  return resolve(Promise.race(racingPromises));
+	return resolve(Promise.race(racingPromises));
 });
--- wait-as-promised.test.js
+++ wait-as-promised.test.js
@@ -1,66 +1,66 @@
 import test from 'ava';
-import wait from '.';
+import wait from './wait-as-promised';
 
 test('should resolve', async t => {
-  let value = false;
+	let value = false;
 
-  const predicate = () => Boolean(value);
+	const predicate = () => Boolean(value);
 
-  setTimeout(() => {
-    value = true;
-  });
+	setTimeout(() => {
+		value = true;
+	});
 
-  await wait(predicate).then(() => t.pass());
+	await wait(predicate).then(() => t.pass());
 });
 
 test('should timeout', async t => {
-  let value = false;
+	let value = false;
 
-  const predicate = () => Boolean(value);
+	const predicate = () => Boolean(value);
 
-  setTimeout(() => {
-    value = true;
-  }, 5100);
+	setTimeout(() => {
+		value = true;
+	}, 5100);
 
-  const error = await t.throws(wait(predicate, {timeout: 500}));
+	const error = await t.throws(wait(predicate, {timeout: 500}));
 
-  t.is(error.message, 'function timed out after 500 milliseconds');
+	t.is(error.message, 'function timed out after 500 milliseconds');
 });
 
 test('should not timeout', async t => {
-  let value = false;
+	let value = false;
 
-  const predicate = () => Boolean(value);
+	const predicate = () => Boolean(value);
 
-  setTimeout(() => {
-    value = true;
-  }, 200);
+	setTimeout(() => {
+		value = true;
+	}, 200);
 
-  await wait(predicate, {timeout: 500}).then(() => t.pass());
+	await wait(predicate, {timeout: 500}).then(() => t.pass());
 });
 
 test('should timeout by default after 5000 milliseconds', async t => {
-  let value = false;
+	let value = false;
 
-  const predicate = () => Boolean(value);
+	const predicate = () => Boolean(value);
 
-  setTimeout(() => {
-    value = true;
-  }, 5100);
+	setTimeout(() => {
+		value = true;
+	}, 5100);
 
-  const error = await t.throws(wait(predicate));
+	const error = await t.throws(wait(predicate));
 
-  t.is(error.message, 'function timed out after 5000 milliseconds');
+	t.is(error.message, 'function timed out after 5000 milliseconds');
 });
 
 test('should not timeout if timeout value is lower than zero', async t => {
-  let value = false;
+	let value = false;
 
-  const predicate = () => Boolean(value);
+	const predicate = () => Boolean(value);
 
-  setTimeout(() => {
-    value = true;
-  }, 5100);
+	setTimeout(() => {
+		value = true;
+	}, 5100);
 
-  await wait(predicate, {timeout: -1}).then(() => t.pass());
+	await wait(predicate, {timeout: -1}).then(() => t.pass());
 });
